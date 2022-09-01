559fe87bcc82da5b8e6fd263d699ed952f44efbc
Gustavo <gustavomlima9393@gmail.com>
2022-09-01 14:28:32 -0300

    Terceiro commit do Blog Pessoal

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -6,43 +6,49 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 @Table(name = "tb_postagens")
+
 public class Postagem {
-    
+
     @Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY) 
-	private Long id;
-	
-	@NotBlank(message = "O atributo título é Obrigatório!") 
-	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
-	private String titulo;
-	
-	@NotBlank(message = "O atributo texto é Obrigatório!")
-	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres")
-	private String texto;
-	
-	@UpdateTimestamp
-	private LocalDateTime data;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
-    /*Insira os Getters and Setters*/
+    @NotBlank(message = "O titulo é obrigatório!!!")
+    @Size(min = 5, max = 100, message = "O titulo deve conter no minimo 10 caracteres e no máximo 100")
+    private String titulo;
 
-    public Long getId() {
-        return this.id;
+    @NotBlank(message = "O texto é obrigatório!!!")
+    @Size(min = 5, max = 1000, message = "O texto deve conter no minimo 10 caracteres e no máximo 1000")
+    private String texto;
+
+    @UpdateTimestamp
+    private LocalDateTime data;
+
+    @ManyToOne
+    @JsonIgnoreProperties("postagem")
+    private Tema tema;
+
+    public long getId() {
+        return id;
     }
 
-    public void setId(Long id) {
+    public void setId(long id) {
         this.id = id;
     }
 
     public String getTitulo() {
-        return this.titulo;
+        return titulo;
     }
 
     public void setTitulo(String titulo) {
@@ -50,7 +56,7 @@ public class Postagem {
     }
 
     public String getTexto() {
-        return this.texto;
+        return texto;
     }
 
     public void setTexto(String texto) {
@@ -58,11 +64,19 @@ public class Postagem {
     }
 
     public LocalDateTime getData() {
-        return this.data;
+        return data;
     }
 
     public void setData(LocalDateTime data) {
         this.data = data;
+    }
+
+    public Tema getTema() {
+        return tema;
+    }
+
+    public void setTema(Tema tema) {
+        this.tema = tema;
     }
 
 }
